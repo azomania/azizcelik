5dec6be2a0f978d6d061f83ea64e3a7a9b312c86
azomania <celikazi@itu.edu.tr>
2025-06-02 22:41:14 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  base: "/<azizcelik>/", // Örnek: "/azizcelik/"
-  ...
+  plugins: [react()]
 })
