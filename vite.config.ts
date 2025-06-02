745732dbc3774f73da904650020b6a5a9d99abca
azomania <celikazi@itu.edu.tr>
2025-06-02 23:00:09 +0300

    base change 2

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/azicelik/',
+  base: '/azizcelik/',
   plugins: [react()]
 })
